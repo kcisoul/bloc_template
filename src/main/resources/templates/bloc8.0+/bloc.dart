16bcd1f788a9ffa9fa05ea9510b5a0f673562702
YooMyounggeun <kciyoo@sk.com>
2023-05-26 16:51:09 +0900

    Template 문법 오류 수정

--- src/main/resources/templates/bloc8.0+/bloc.dart
+++ src/main/resources/templates/bloc8.0+/bloc.dart
@@ -6,7 +6,7 @@ class $nameBloc extends Bloc<$nameEvent, $nameState> {
   $nameBloc() : super($nameStateNone()) {
     on<$nameEventInit>(_init);
     on<$nameEventLoadData>(_loadData);
-    on<$nameEventLoadData>(_reload);
+    on<$nameEventReload>(_reload);
   }
 
   void _init($nameEventInit event, Emitter<$nameState> emit) async {
